3717016d9bfc93727691a6b4eea28ad9ed55fe5e
julien <julienbraine@yahoo.fr>
2023-07-21 15:20:45 +0200

    update

--- src/RessourceManager/storages.py
+++ src/RessourceManager/storages.py
@@ -7,10 +7,9 @@ Module defining abstract storages. There are two main classes that communicate u
         This class is responsible for creating the IO object (for example the :meth:`pathlib.Path.open`) at the right location.
     - :class:`AbstractIOProtocol` which handles how a python variable is exported/loaded to/from data (e.g. binary data, string, ...) to an IO Object.
 
-They both have a listed set of `IO` objects with which they are compatible (called io_method). 
+They both have a listed set of `io_methods` with which they are compatible. 
 The user of this module needs to find an io_method compatible with both ends. 
-Then the tuple (:class:`AbstractStorage`, IO object, :class:`AbstractIOProtocol`) defines a storage solution to load and dump variables.
-The tuple is usually wrapped in the :class:`StorageSolution` class.
+Then the tuple (:class:`AbstractStorage`, io_method, :class:`AbstractIOProtocol`) defines a storage solution to load and dump variables.
 
 """
 
@@ -23,14 +22,11 @@ import io, pathlib, multiprocessing, inspect, functools
 StorageLocation = TypeVar("StorageLocation")
 AlphaNumString = NewType("AlphaNumString", str)
 IO = NewType("IO", Any)
-class LifetimeProtocol(Protocol):
-    def persists_after_execution(self) -> bool: ...
 
 
-
-class AbstractStorage(Generic[StorageLocation]):
+class AbstractStorage(Generic[StorageLocation], Protocol):
     """
-    Abstract base class for storage solutions.
+    Abstract base class representing a storage.
     A storage may be a directory on disk, a directory on a server, a database, an hdf5 file, but also a dictionary in memory, a dictionary in memory in a different process, ...
 
     This class is meant to interact well with the AbstractIOProtocol class and the AbstractImportExportFormat class.
@@ -44,13 +40,13 @@ class AbstractStorage(Generic[StorageLocation]):
     ----------
         - define move function within and between storages. May require double_dispatch. Unclear whether this should be a free function, a static method or a method.
     """
-    
+
     name: str
 
     def __init__(self, name):
         self.name = name
 
-    def get_ressource_location(self, uniqueid: AlphaNumString, name_info: Any = None) -> StorageLocation:
+    def get_ressource_location(self, uniqueid: AlphaNumString, name_info: Any = None) -> StorageLocation: 
         """
         Returns where a ressource with uniqueid and name_info is stored on this storage. In other words, it transforms a unique_id into a location on this storage.
         This enables one to use the same uniqueid for different storages, and retrieve the corresponding location for each storage.
@@ -81,7 +77,7 @@ class AbstractStorage(Generic[StorageLocation]):
         StorageLocation
             The location of the ressource identified with uniqueid on this storage.
         """
-        raise NotImplementedError()
+        ...
 
     def has_ressource(self, location: StorageLocation) -> bool:
         """
@@ -97,7 +93,7 @@ class AbstractStorage(Generic[StorageLocation]):
         bool
            whether a ressource is saved at location location.
         """
-        raise NotImplementedError()
+        ...
     
     def mk_write_io(self, io_method: str, location: StorageLocation) -> IO:
         """
@@ -119,7 +115,7 @@ class AbstractStorage(Generic[StorageLocation]):
         Any
            the IO object desired by the io_method
         """
-        raise NotImplementedError()
+        ...
     
     def mk_read_io(self, io_method: str, location: StorageLocation) -> IO:
         """
@@ -141,7 +137,7 @@ class AbstractStorage(Generic[StorageLocation]):
         Any
            the IO object desired by the io_method
         """
-        raise NotImplementedError()
+        ...
     
     def supports_iomethod(method: str) -> bool:
         """
@@ -154,7 +150,7 @@ class AbstractStorage(Generic[StorageLocation]):
         -------
         :method:`mk_read_io`, :method:`mk_write_io`, :class:`IOProtocol` 
         """
-        raise NotImplementedError()
+        ...
     
     def remove(self, locations: Set[StorageLocation], missing_ok=False) -> NoReturn:
         """
@@ -168,6 +164,29 @@ class AbstractStorage(Generic[StorageLocation]):
         """
         raise NotImplementedError()
     
+    @property
+    def space(self) -> SpaceInformation:
+        """
+        Returns
+        -------
+        SpaceInformation
+           the space information about the storage, mainly the methods :meth:`SpaceInformation.used_space` `SpaceInformation.remaining_space`.
+        """
+    
+    @property
+    def lifetime(self) -> LifetimeInformation:
+        """
+        Returns
+        -------
+        LifetimeInformation
+           the lifetime information about the storage mainly the method `persists_after_execution` indicating whether the storage diseapears after execution.
+        """
+        ...
+
+class LifetimeInformation(Protocol):
+    def persists_after_execution(self) -> bool: ...
+
+class SpaceInformation(Protocol):
     def remaining_space(self) -> float:
         """
         Returns
@@ -180,11 +199,11 @@ class AbstractStorage(Generic[StorageLocation]):
             The remaning space of a storage may be shared with another storage, 
             for example when one has two storages defined on two different folders of the same disk.
         """
-        raise NotImplementedError()
+        ...
     
     def used_space(self, shared=False) -> float:
         """
-         Parameters
+        Parameters
         ----------
             shared: bool, default=False
                 Only makes a difference if the storage is shared with other storages/systems/...
@@ -200,11 +219,79 @@ class AbstractStorage(Generic[StorageLocation]):
         """
         raise NotImplementedError()
     
-    def lifetime(self) -> LifetimeProtocol:
+class IOProtocol(Protocol):
+    """
+    Abstract base class representing a way to export/import python variables to storage.
+    It handles the encoding and does the actual writing and reading of the data.
+
+    Attributes
+    ----------
+        name: str
+            **Should be unique for every protocol**. This is extremely important the same ressource exported using two different protocols (imagine a dataframe exported either with csv or parquet)
+            should be treated as different different ressources on storage. This is usually done by passing the name of the protocol in the uniqueid used to generate the location of the ressource.
+
+    """
+     
+    name: str
+
+    def __init__(self, name):
+        self.name = name
+
+    def supports_dtype_method(t : type, io_method: str) -> bool:
         """
         Returns
         -------
-        LifetimeProtocol
-           the lifetime information about the storage. This object has the method `persists_after_execution` indicating whether the storage diseapears after execution.
+        bool
+           whether the given dtype is supported on the given io method. 
+
+        Note 
+        -------
+        Many IOProtocols may simply not support some types (or even only handle a single type).
+        In that case, it simply returns False for all io_method
+
+
+        See Also
+        -------
+        :method:`load`, :method:`dump`, :class:`AbstractStorage` 
         """
-        raise NotImplementedError()
+
+    def dump(self, io: IO, obj: Any) -> NoReturn:
+        """
+        Dumps the object into ìo`, thus writing the object to storage (the location was determined when creating `io`)
+        Parameters
+        ----------
+            io: IO
+                The `io` object created by the storage with :meth:`AbstractStorage.mk_write_io`
+            obj: Any
+                The object to dump
+
+        Notes
+        ----------
+        - The object must be of a supported type and the io must have been created with a supported io_method.
+        Currently load does not take the io_method as a parameter. It is assumed that the needed information (possible the name of the method)
+        is included in the IO object. It is yet unclear whether this is good design
+
+        - It is assumed that any "closing" of handles (ssh connection, file handle, ...) inside IO is done here
+        """
+        ...
+    def load(self, io: IO) -> Any:
+        """
+        Load the object from ìo`, thus reading the object from storage (the location was determined when creating `io`)
+        Parameters
+        ----------
+            io: IO
+                The `io` object created by the storage with :meth:`AbstractStorage.mk_read_io`
+
+        Returns
+        -------
+        obj: Any
+                The loaded object
+        Notes
+        ----------
+        - The io must have been created with a supported io_method.
+        Currently load does not take the io_method as a parameter. It is assumed that the needed information (possible the name of the method)
+        is included in the IO object. It is yet unclear whether this is good design
+
+        - It is assumed that any "closing" of handles (ssh connection, file handle, ...) inside IO is done here
+        """
+        ...
