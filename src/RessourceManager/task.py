707f4590900a677c2f17270b5a777a5f8ad2a305
julien <julienbraine@yahoo.fr>
2023-08-29 17:41:10 +0200

    update

--- src/RessourceManager/task.py
+++ src/RessourceManager/task.py
@@ -106,19 +106,44 @@ class Task:
     used_by: List[Task] = []
     
     manager: TaskManager
-    computation_status: Literal["running", "loading", "none"] = "none"
     
     @functools.cached_property #For each parameter the list of tasks in it
-    def task_dependencies(self) -> Dict[str, List[Task]]: raise NotImplementedError 
+    def task_dependencies(self) -> Dict[str, List[Task]]: 
+        return {k:o.embedded_task_retriever(v)[0] for k,(v,o) in self.param_dict if not o.dependency == "ignore"}
 
     @functools.cached_property
-    def identifier(self): raise NotImplementedError
+    def identifier(self): 
+        def get_param_id(v, o: TaskParamOptions):
+            (l, reconstruct) = o.embedded_task_retriever(v)
+            return unique_id(reconstruct([task.identifier for task in l]))
+
+        params_id = {k:get_param_id(v, o) for k,(v,o) in self.param_dict if not o.dependency == "ignore"}
+        return make_result_id(self.group.name, params_id, False)
     
     @functools.cached_property
-    def storage_id(self): raise NotImplementedError
+    def storage_id(self):
+        def get_param_id(v, o: TaskParamOptions):
+            (l, reconstruct) = o.embedded_task_retriever(v)
+            match o.dependency:
+                case "graph":
+                    return unique_id(reconstruct([task.storage_id for task in l]))
+                case "value":
+                    return unique_id(reconstruct([task.result() for task in l]))
+                case _: 
+                    raise ValueError(f"Unknown dependency option {o.dependency}")
+                
+        params_id = {k:get_param_id(v, o) for k,(v,o) in self.param_dict if not o.dependency == "ignore"}
+        return make_result_id(self.group.name, params_id, True)
 
     @functools.cached_property
-    def short_name(self): raise NotImplementedError
+    def short_name(self): 
+        self.identifier[0:50] + ('...)' if len(self.identifier) > 49 else '')
+    
+    def __repr__(self):
+        return self.identifier
+    
+    def __str__(self):
+        return self.short_name
     
     def write_on_storage(self, s: Storage, progress = tqdm.tqdm): raise NotImplementedError
     def result(self, exception: (Literal["raise", "return"] | List[Exception]) = "raise", progress=tqdm.tqdm): raise NotImplementedError
@@ -128,7 +153,23 @@ class Task:
     def get_history(self) -> pd.DataFrame: raise NotImplementedError
     def get_stats(self) -> pd.DataFrame: raise NotImplementedError
 
-    def _compute(self) -> NoReturn: raise NotImplementedError
+    def _compute(self) -> NoReturn: 
+        def get_param_values(v, o: TaskParamOptions):
+            (l, reconstruct) = o.embedded_task_retriever(v)
+            match o.pass_as, o.exception:
+                case "value", "propagate":
+                    return reconstruct([t.result(exception="raise") for t in l])
+                case "value", "return":
+                    return reconstruct([t.result(exception="return") for t in l])
+                case "task", "computed":
+                    pass
+                case "task", "uncomputed":
+                    pass
+                case "location", locs:
+                    pass
+                case _: 
+                    raise ValueError(f"Unknown pass_as option {o.pass_as}")
+            return unique_id(reconstruct([task.storage_id if o.dependency == "graph" else task.result() for task in l]))
 
 
 class TaskManager:
